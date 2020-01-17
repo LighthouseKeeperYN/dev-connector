0fe65eb5201824c823e69ac77e3f2c1e3440f96c
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-17 14:06:12 +0300

    feat: user schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/user
+// @route   GET api/users
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('user route'));
+router.get('/', (req, res) => res.send('users route'));
 
 module.exports = router;
