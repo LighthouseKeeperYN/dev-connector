79e6a916378828201c956d061c56388177b4937c
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-18 13:03:42 +0300

    feat: profile route put experience request

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,9 +24,7 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+    if (!errors.isEmpty()) return res.status(400).json(errors);
 
     const { name, email, password } = req.body;
 
