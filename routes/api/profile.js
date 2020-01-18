b4aa2d75c2ead5fb45088f12fa03575f63233de2
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-18 13:18:50 +0300

    feat: profile route delete experience request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -187,4 +187,23 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.experience = profile.experience.filter(
+      (item) => item.id !== req.params.exp_id
+    );
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    sendServerError(err, res);
+  }
+});
+
 module.exports = router;
