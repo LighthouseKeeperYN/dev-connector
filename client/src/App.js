b0e446be0800fb9d13e5bc79aee758a33b1de3aa
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-18 20:54:38 +0300

    feat: landing page markup, navbar markup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import 'normalize.css';
-import './App.scss';
+import './App.css';
 
-const App = () => <div></div>;
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+const App = () => (
+  <BrowserRouter>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </BrowserRouter>
+);
 
 export default App;
