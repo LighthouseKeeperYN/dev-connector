af63a5c8a1a709292cd8ab6ddd8f895a2f93041b
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-18 18:56:45 +0300

    feat: posts route delete comment request

--- models/Post.js
+++ models/Post.js
@@ -11,6 +11,7 @@ const PostSchema = new Schema({
     {
       user   : { type: Schema.Types.ObjectId, ref: 'users' },
       text   : { type: String, required: true },
+      name : { type: String },
       avatar : { type: String },
       date   : { type: Date, default: Date.now },
     },
