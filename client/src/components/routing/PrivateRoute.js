0b8c73ba896ac5a567711450378422669481122c
LighthouseKeeperYN <freesaber@yandex.ru>
2020-01-19 17:31:48 +0300

    feat: private route

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+function PrivateRoute({ component: Component, auth, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!auth.isAuthenticated && !auth.loading) return <Redirect to="/login" />;
+        else return <Component {...props} />;
+      }}
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,6 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-
 const middleWare = [thunk];
 
 const store = createStore(
